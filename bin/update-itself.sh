92553a5dc33f970e1ca2922ed482fe27b4dcc51a
Roman Chyla <roman.chyla@gmail.com>
2017-06-05 17:31:07 -0400

    Updated itself

---
+++ bin/update-itself.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# purpose of this script is to automatically update the github
+# repository
+
+
+bin_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+
+cd $bin_dir/..
+git pull
+
+if [[ "$?" != "0" ]]; then
+  echo "Problem updating repository at: " $(cd $bin_dir/.. && pwd)
+fi
