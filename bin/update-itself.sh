74302b122624f46bcf57ace5b91d1e13fbebca32
Roman Chyla <roman.chyla@gmail.com>
2017-06-05 17:40:18 -0400

    Avoid being verbose

--- bin/update-itself.sh
+++ bin/update-itself.sh
@@ -7,7 +7,7 @@
 bin_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
 cd $bin_dir/..
-git pull
+git pull > /dev/null
 
 if [[ "$?" != "0" ]]; then
   echo "Problem updating repository at: " $(cd $bin_dir/.. && pwd)
